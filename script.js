7885bbd0cce2da1101eddcf58425d7a4f3764505
Emiliano <emilianohernan92@gmail.com>
2024-07-16 16:04:52 -0300

    Cambios  en el pdf

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ document.addEventListener('DOMContentLoaded', async function () {
         const instructor = document.getElementById('instructor').value;
         const direccion = document.getElementById('direccion').value;
         const centroformacion = document.getElementById('centroformacion').value;
+        const dni = document.getElementById('dni').value;
+        const nivelOperario = document.getElementById('nivelOperario').value;
 
         if (!nombre || !curso || !ingreso || !salida || !instructor || !direccion || !centroformacion) {
             alert('Todos los campos son Obligatorios');
             return;
         }
 
-        // Lógica para generar el certificado PDF utilizando los datos del formulario
-        const pdfBytes = await generateCustomCertificate(nombre, curso, ingreso, salida, instructor, direccion, centroformacion);
+
+        //Lógica para generar el certificado PDF utilizando los datos del formulario
+        const pdfBytes = await generateCustomCertificate(nombre, curso, ingreso, salida, instructor, direccion, centroformacion,dni, nivelOperario);
 
         // Descargar el certificado generado
         if (pdfBytes) {
@@ -31,7 +34,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         }
     }
 
-    async function generateCustomCertificate(nombre, curso, ingreso, salida, instructor, direccion, centroformacion) {
+    async function generateCustomCertificate(nombre, curso, ingreso, salida, instructor, direccion, centroformacion,dni, nivelOperario) {
         // Usar fetch para obtener el archivo PDF base
         const response = await fetch('certificadoprueba.pdf'); // Reemplaza 'certificadoprueba.pdf' con tu archivo base
         const arrayBuffer = await response.arrayBuffer();
@@ -40,10 +43,11 @@ document.addEventListener('DOMContentLoaded', async function () {
         const pdfDoc = await PDFLib.PDFDocument.load(arrayBuffer);
 
         // Cargar las fuentes estándar de PDF-LIB
-        const [helveticaBoldFont, helveticaFont, timesRomanFont] = await Promise.all([
+        const [helveticaBoldFont, helveticaFont, timesRomanFont, helveticaItalicFont] = await Promise.all([
             pdfDoc.embedFont(PDFLib.StandardFonts.HelveticaBold),
             pdfDoc.embedFont(PDFLib.StandardFonts.Helvetica),
             pdfDoc.embedFont(PDFLib.StandardFonts.TimesRoman),
+            pdfDoc.embedFont(PDFLib.StandardFonts.HelveticaBoldOblique),
         ]);
 
         // Modificar el PDF según los datos del formulario
@@ -51,60 +55,80 @@ document.addEventListener('DOMContentLoaded', async function () {
         const firstPage = pages[0];
         const { width, height } = firstPage.getSize();
 
+
+
         // Ejemplo de modificación (ajustar según tu necesidad)
         firstPage.drawText(nombre, {
-            x: 50,
-            y: 550,
-            size: 20,
+            x: 150,
+            y: 350,
+            size: 40,
             font: helveticaBoldFont,
             color: PDFLib.rgb(0, 0, 0),
         });
 
         firstPage.drawText(curso, {
-            x: 50,
-            y: 520,
-            size: 18,
+            x: 130,
+            y: 430,
+            size: 60,
             font: helveticaBoldFont,
             color: PDFLib.rgb(0, 0, 0),
         });
 
-        firstPage.drawText(ingreso, {
-            x: 50,
-            y: 490,
-            size: 18,
-            font: timesRomanFont,
-            color: PDFLib.rgb(0, 0, 0),
-        });
-
-        firstPage.drawText(salida, {
-            x: 50,
-            y: 460,
-            size: 18,
-            font: timesRomanFont,
-            color: PDFLib.rgb(0, 0, 0),
-        });
-
-        firstPage.drawText(instructor, {
-            x: 50,
-            y: 430,
-            size: 18,
+        const textoDni= `DNI: ${dni}` 
+        firstPage.drawText(textoDni, {
+            x: 370,
+            y: 310,
+            size: 17,
             font: helveticaFont,
+            color: PDFLib.rgb(0, 0, 0),
+        });
+
+        const emision= `Acreditacion Profesional AATTVAC Reg. N°: 0450 - Fecha Emision: ${ingreso} - Expira: ${salida}`
+        firstPage.drawText(emision, {
+            x: 130,
+            y: 200,
+            size: 15,
+            font: timesRomanFont,
+            color: PDFLib.rgb(0, 0, 0),
+        });
+
+        const nivel= `Fue calificado como ${nivelOperario}: Profesional en Acceso por Cuerdas Industrial (40HS).`
+        firstPage.drawText(nivel, {
+            x: 140,
+            y: 290,
+            size: 13,
+            font: helveticaFont,
+            color: PDFLib.rgb(0, 0, 0),
+        });
+
+        const formacion= `Dictado en Centro de formacion AATTVAC, Pcia de ${centroformacion}, Argentina.`
+        firstPage.drawText(formacion, {
+            x: 200,
+            y: 270,
+            size: 13,
+            font: helveticaBoldFont,
+            color: PDFLib.rgb(0, 0, 0),
+        });
+
+        // Posición X deseada para que el texto termine en (por ejemplo, 50 píxeles desde la derecha)
+        const desiredRightMargin = 100;
+        // Calcular el ancho del texto
+        const instructorTextWidth = helveticaBoldFont.widthOfTextAtSize(instructor, 10);
+        // Calcular la posición X
+        const instructorX = width - desiredRightMargin - instructorTextWidth;
+        firstPage.drawText(instructor, {
+            x: instructorX,
+            y: 103,
+            size: 10,
+            font: helveticaItalicFont,
             color: PDFLib.rgb(0, 0, 0),
         });
 
         firstPage.drawText(direccion, {
-            x: 50,
-            y: 400,
-            size: 18,
-            font: helveticaFont,
-            color: PDFLib.rgb(0, 0, 0),
-        });
-
-        firstPage.drawText(centroformacion, {
-            x: 50,
-            y: 370,
-            size: 18,
-            font: helveticaFont,
+            x: 140,
+            y: 100,
+            size: 10,
+            font: helveticaItalicFont,
             color: PDFLib.rgb(0, 0, 0),
         });
 
