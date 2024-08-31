f932459783ebe24ab7ca03373145d0328f580925
Sjalok <franco_culotta97@hotmail.com>
2024-08-31 12:24:04 -0300

    pasaron cosas

--- produccion/certificados.js
+++ produccion/certificados.js
@@ -7,18 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             serverPassword = data.password;
-            console.log('Contraseña del servidor:', serverPassword);
         })
         .catch(error => console.error('Error fetching password:', error));
 
     formulario.addEventListener('submit', (e) => {
         const contraseña = campoPassword.value;
 
-        console.log('Contraseña ingresada:', contraseña);
-        console.log('Contraseña del servidor:', serverPassword);
-
         if (contraseña === serverPassword) {
-            console.log("asd");
             window.location.href = `/formulario`;
         } else {
             alert('Contraseña incorrecta');
